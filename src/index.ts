81671c820a15acf4a53b7845c89509ed40bf183d
Andrea Silvi <andrea.silvi94@gmail.com>
2023-02-22 22:57:53 +0100

    Encoded wasm in base64

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export * from "./cpu";
-export * from "./wasm";
+import * as cpu from "./cpu";
+import * as wasm from "./wasm";
+
+export default function(backend: "cpu" | "wasm") {
+    switch (backend) {
+        case "cpu":
+            return cpu;
+        case "wasm":
+            return wasm;
+    }
+}
 
--- src/wasm/index.ts
+++ src/wasm/index.ts
@@ -1,7 +1,7 @@
-import * as ABI from "../../dist/wasm/asc";
-import fs from "fs";
+import * as ABI from "./asc/build";
+import wasm from "./asc/build/index.wasm.b64"
 
 export default async function (): Promise<typeof ABI.__AdaptedExports> {
-    const bytes = fs.readFileSync("./dist/wasm/asc/index.wasm");
+    const bytes = Buffer.from(wasm, 'base64');
     return ABI.instantiate(await WebAssembly.compile(bytes), {env: {}});
 }
