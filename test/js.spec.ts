8709f04d2e3e5ee3a72ea7032050e0763544c400
Andrea Silvi <andrea.silvi94@gmail.com>
2023-02-24 02:00:06 +0100

    Fixed tests

--- test/js.spec.ts
+++ test/js.spec.ts
@@ -1,10 +1,10 @@
-import * as cpu from "../src/backend/js";
+import backend from "../src/backend/js";
 import {NdArray} from "../src"
 
 describe("Typescript tests", async () => {
 
     it("Check 1", async () => {
-        const nt = await cpu.default();
+        const nt = await backend();
         const a = nt.NdInt8ArrayNew(new Int8Array([1, 2, 3, 4, 5, 6]), new Uint32Array([2, 3]));
         const b = nt.NdInt8ArrayNew(new Int8Array([-1, -2, -3, -4, -5, -6]), new Uint32Array([2, 3]));
         console.log(nt.NdInt8ArrayGetData(a));
--- test/wasm.spec.ts
+++ test/wasm.spec.ts
@@ -1,15 +1,15 @@
-import * as nt from "../src/backend/wasm";
+import backend from "../src/backend/wasm";
 import {NdArray} from "../src";
 
 describe("ASC tests", async () => {
 
     it("Check 1", async () => {
-        const nt1 = await nt.default();
-        const a = nt1.NdInt8ArrayNew(new Int8Array([1, 2, 3, 4, 5, 6]), new Uint32Array([2, 3]));
-        const b = nt1.NdInt8ArrayNew(new Int8Array([-1, -2, -3, -4, -5, -6]), new Uint32Array([2, 3]));
-        console.log(nt1.NdInt8ArrayGetData(a));
-        nt1.NdInt8ArrayAdd(a, b);
-        console.log(nt1.NdInt8ArrayGetData(a));
+        const nt = await backend();
+        const a = nt.NdInt8ArrayNew(new Int8Array([1, 2, 3, 4, 5, 6]), new Uint32Array([2, 3]));
+        const b = nt.NdInt8ArrayNew(new Int8Array([-1, -2, -3, -4, -5, -6]), new Uint32Array([2, 3]));
+        console.log(nt.NdInt8ArrayGetData(a));
+        nt.NdInt8ArrayAdd(a, b);
+        console.log(nt.NdInt8ArrayGetData(a));
     }).timeout(60000);
 
     it("Check 2", async () => {
